95c9fd442535d77b6847a26033dd2e8bb0a0fab3
ChaoYue <chaoyuejoy@gmail.com>
2013-04-05 10:54:16 +0200

    add test for test_find_index_by_point

--- gnc_test.py
+++ gnc_test.py
@@ -42,10 +42,11 @@ class test_Add_Vars_to_Dict_by_RegSum():
 
         #testing sum; area_weight as None
         litcon1 = d.Add_Vars_to_Dict_by_RegSum(['LITTER_CONSUMP'],mode='sum')
-        np.testing.assert_allclose(litcon1['LITTER_CONSUMP'],np.ma.sum(np.ma.sum(litter_consump,axis=-1),axis=-1))
+        np.testing.assert_allclose(litcon1['LITTER_CONSUMP'],np.ma.sum(np.ma.sum(litter_consump,axis=-1),axis=-1),rtol=1E-5)
         #testing mean; area_weight as None
         litcon1 = d.Add_Vars_to_Dict_by_RegSum(['LITTER_CONSUMP'],mode='mean')
-        np.testing.assert_allclose(litcon1['LITTER_CONSUMP'],np.ma.sum(np.ma.sum(litter_consump,axis=-1),axis=-1)/gridcell_num)
+            #we set rtol as high as the mean values are all small ones.
+        np.testing.assert_allclose(litcon1['LITTER_CONSUMP'],np.ma.sum(np.ma.sum(litter_consump,axis=-1),axis=-1)/gridcell_num,rtol=1E-3)
 
         #testing sum; area_weight as True
         litter_consump_area = area*litter_consump
@@ -55,7 +56,7 @@ class test_Add_Vars_to_Dict_by_RegSum():
         total_area = np.ma.sum(area)
         litter_consump_area = area*litter_consump
         litcon1 = d.Add_Vars_to_Dict_by_RegSum(['LITTER_CONSUMP'],mode='mean',area_weight=True)
-        np.testing.assert_allclose(litcon1['LITTER_CONSUMP'],np.ma.sum(np.ma.sum(litter_consump_area,axis=-1),axis=-1)/total_area)
+        np.testing.assert_allclose(litcon1['LITTER_CONSUMP'],np.ma.sum(np.ma.sum(litter_consump_area,axis=-1),axis=-1)/total_area,rtol=1E-3)
 
     def test_grid(self):
 
@@ -69,10 +70,10 @@ class test_Add_Vars_to_Dict_by_RegSum():
 
         #testing sum; area_weight as None
         litcon1 = d.Add_Vars_to_Dict_by_RegSum(['LITTER_CONSUMP'],mode='sum',grid=grid)
-        np.testing.assert_allclose(litcon1['LITTER_CONSUMP'],np.ma.sum(np.ma.sum(litter_consump,axis=-1),axis=-1))
+        np.testing.assert_allclose(litcon1['LITTER_CONSUMP'],np.ma.sum(np.ma.sum(litter_consump,axis=-1),axis=-1),rtol=1E-5)
         #testing mean; area_weight as None
         litcon1 = d.Add_Vars_to_Dict_by_RegSum(['LITTER_CONSUMP'],mode='mean',grid=grid)
-        np.testing.assert_allclose(litcon1['LITTER_CONSUMP'],np.ma.sum(np.ma.sum(litter_consump,axis=-1),axis=-1)/gridcell_num)
+        np.testing.assert_allclose(litcon1['LITTER_CONSUMP'],np.ma.sum(np.ma.sum(litter_consump,axis=-1),axis=-1)/gridcell_num,rtol=1E-3)
 
         #testing sum; area_weight as True
         litter_consump_area = area*litter_consump
@@ -82,6 +83,59 @@ class test_Add_Vars_to_Dict_by_RegSum():
         total_area = np.ma.sum(area)
         litter_consump_area = area*litter_consump
         litcon1 = d.Add_Vars_to_Dict_by_RegSum(['LITTER_CONSUMP'],mode='mean',area_weight=True,grid=grid)
-        np.testing.assert_allclose(litcon1['LITTER_CONSUMP'],np.ma.sum(np.ma.sum(litter_consump_area,axis=-1),axis=-1)/total_area)
+        np.testing.assert_allclose(litcon1['LITTER_CONSUMP'],np.ma.sum(np.ma.sum(litter_consump_area,axis=-1),axis=-1)/total_area,rtol=1E-5)
+
+
+
+class test_find_index_by_point():
+    lat = np.arange(4.75,0,-0.5)
+    lon = np.arange(0.25,5,0.5)
+    @ntools.raises(ValueError)
+    def test_decrease_lat(self):
+        """
+        increasing lat input should raise ValueError.
+        """
+        lat_increase = np.arange(0.25,5,0.5)
+        lon = np.arange(0.25,5,0.5)
+        gnc.find_index_by_point(lat_increase,lon,(2,2))
+    @ntools.raises(ValueError)
+    def test_big_value_lat(self):
+        """
+        vlat/vlon bigger than range should raise ValueError.
+        """
+        gnc.find_index_by_point(self.lat,self.lon,(5.01,2))
+
+    @ntools.raises(ValueError)
+    def test_small_value_lat(self):
+        """
+        vlat/vlon smaller than range should raise ValueError.
+        """
+        gnc.find_index_by_point(self.lat,self.lon,(-0.01,2))
+
+    @ntools.raises(ValueError)
+    def test_big_value_lon(self):
+        """
+        vlat/vlon bigger than range should raise ValueError.
+        """
+        gnc.find_index_by_point(self.lat,self.lon,(2,5.01))
+
+    @ntools.raises(ValueError)
+    def test_small_value_lon(self):
+        """
+        vlat/vlon smaller than range should raise ValueError.
+        """
+        gnc.find_index_by_point(self.lat,self.lon,(2,-0.01))
+
+    def test_return_correct_value(self):
+        ntools.assert_tuple_equal((0,0),gnc.find_index_by_point(self.lat,self.lon,(5,0)))
+        ntools.assert_tuple_equal((9,0),gnc.find_index_by_point(self.lat,self.lon,(0,0)))
+        ntools.assert_tuple_equal((9,9),gnc.find_index_by_point(self.lat,self.lon,(0,5)))
+        ntools.assert_tuple_equal((0,9),gnc.find_index_by_point(self.lat,self.lon,(5,5)))
+        ntools.assert_tuple_equal((2,7),gnc.find_index_by_point(self.lat,self.lon,(3.99,3.99)))
+
+
+
+
+
 
 
